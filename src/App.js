79d766512659ef63d4755c43fb1b6ccc756f4b37
sunny <sunnyvales789@gmail.com>
2024-10-26 14:32:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './Header';
 function App() {
+ // https://tailwindcss.com/docs
   return (
 <div className="container mx-auto mb-8 px-8">
 <Header />
